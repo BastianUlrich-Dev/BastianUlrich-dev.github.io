2b2213deca88194a8c21e2e77b2f25b21164672d
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-18 18:47:13 -0400

    creacion componente portada

---
+++ src/app/pages/home/portada/portada.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portada',
+  templateUrl: './portada.component.html',
+  styleUrls: ['./portada.component.scss']
+})
+export class PortadaComponent {
+
+  imgProfile:string = "../../../assets/img/imgprofile.png";
+  imgAngular:string = "../../../assets/img/angular.png"
+  imgTypescript:string = "../../../assets/img/typescript.png"
+  imgCss:string = "../../../assets/img/css3.png"
+  imgHtml:string = "../../../assets/img/html.png"
+}
