d13452c4e62f2669ddda92932aac0d4a8d36568d
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-18 18:46:43 -0400

    importacion portada component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PortadaComponent } from './pages/home/portada/portada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule,
