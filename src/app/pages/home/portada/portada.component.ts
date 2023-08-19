08a7addb5373c59674184129ed7e06110a238e06
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-19 00:30:13 -0400

    quita lineas

--- src/app/pages/home/portada/portada.component.ts
+++ src/app/pages/home/portada/portada.component.ts
@@ -7,14 +7,9 @@ import { Component } from '@angular/core';
 })
 export class PortadaComponent {
 
-  imgProfile:string = "../../../portafolio-dev.github.io/assets/img/imgprofile.png";
-  imgAngular:string = "../../../portafolio-dev.github.io/assets/img/angular.png"
-  imgTypescript:string = "../../../portafolio-dev.github.io/assets/img/typescript.png"
-  imgCss:string = "../../../portafolio-dev.github.io/assets/img/css3.png"
-  imgHtml:string = "../../../portafolio-dev.github.io/assets/img/html.png"
-  // imgProfile:string = "../../../assets/img/imgprofile.png";
-  // imgAngular:string = "../../../assets/img/angular.png"
-  // imgTypescript:string = "../../../assets/img/typescript.png"
-  // imgCss:string = "../../../assets/img/css3.png"
-  // imgHtml:string = "../../../assets/img/html.png"
+  imgProfile:string = "../../../assets/img/imgprofile.png";
+  imgAngular:string = "../../../assets/img/angular.png"
+  imgTypescript:string = "../../../assets/img/typescript.png"
+  imgCss:string = "../../../assets/img/css3.png"
+  imgHtml:string = "../../../assets/img/html.png"
 }
