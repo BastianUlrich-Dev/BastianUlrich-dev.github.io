80ce4ce5fe1f8447ec0a3d74b45174cab9dd5c1c
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-18 23:01:44 -0400

    importacion de componente experiencia y proyects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PortadaComponent } from './pages/home/portada/portada.component';
+import { ExperienciaComponent } from './pages/home/experiencia/experiencia.component';
+import { ProyectsComponent } from './pages/home/proyects/proyects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    PortadaComponent
+    PortadaComponent,
+    ExperienciaComponent,
+    ProyectsComponent
   ],
   imports: [
     BrowserModule,
