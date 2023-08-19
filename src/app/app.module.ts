d8c37b8fa29d0f575e822493d5dc658a69bfd608
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-19 16:48:53 -0400

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PortadaComponent } from './pages/home/portada/portada.component';
 import { ExperienciaComponent } from './pages/home/experiencia/experiencia.component';
 import { ProyectsComponent } from './pages/home/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProyectsComponent } from './pages/home/proyects/proyects.component';
     HomeComponent,
     PortadaComponent,
     ExperienciaComponent,
-    ProyectsComponent
+    ProyectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
