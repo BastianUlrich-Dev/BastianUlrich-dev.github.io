54d12fc47ee69c04bce923e6c318be48fd2f4679
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-21 15:38:09 -0400

    importacion componente skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PortadaComponent } from './pages/home/portada/portada.component';
 import { ExperienciaComponent } from './pages/home/experiencia/experiencia.component';
 import { ProyectsComponent } from './pages/home/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './pages/home/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './components/footer/footer.component';
     PortadaComponent,
     ExperienciaComponent,
     ProyectsComponent,
-    FooterComponent
+    FooterComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
