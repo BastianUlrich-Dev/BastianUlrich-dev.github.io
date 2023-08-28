12642f033860d28312df03a1aff070ec5845e471
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-08-27 22:59:00 -0400

    ajustes a11y

--- src/app/pages/home/skills/skills.component.ts
+++ src/app/pages/home/skills/skills.component.ts
@@ -48,4 +48,39 @@ export class SkillsComponent {
   imgTypescript:string = "../../../assets/img/typescript.png";
   imgCss:string = "../../../assets/img/css3.png";
   imgHtml:string = "../../../assets/img/html.png";
+  divShadown:boolean = false;
+  begg1:boolean = false;
+  begg2:boolean = true;
+  begg3:boolean = true;
+  begg4:boolean = true;
+  begg5:boolean = true;
+  begg6:boolean = true;
+  activarEgg1(){
+    this.begg2 = false;
+    this.begg1 = true;
+  }
+  activarEgg2(){
+    this.begg3 = false;
+    this.begg2 = true;
+  }
+  activarEgg3(){
+    this.begg4 = false;
+    this.begg3 = true;
+  }
+  activarEgg4(){
+    this.begg5 = false;
+    this.begg4 = true;
+  }
+  activarEgg5(){
+    this.begg6 = false;
+    this.begg5 = true;
+  }
+  activarEgg6(){
+    this.begg6 = true;
+    this.begg1 = false;
+    this.divShadown = true;
+  setTimeout(() => {
+    this.divShadown = false;
+  }, 2000);
+  }
 }
