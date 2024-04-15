649f27913e524628e3210e2df64efb9e75061820
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2024-04-14 23:09:12 -0400

    agrega etiquetas html

--- src/app/pages/proyecto04/proyecto04.component.ts
+++ src/app/pages/proyecto04/proyecto04.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class Proyecto04Component {
 
+  buttonInfo: number | null = null;
+  listInfo= [
+    {
+      title: "¿Qué es Netflix?",
+      description: "Netflix es un servicio de streaming que ofrece una gran variedad de películas, series y documentales premiados en casi cualquier pantalla conectada a internet.",
+      description2: "Todo lo que quieras ver, sin límites ni comerciales, a un costo muy accesible. Siempre hay algo nuevo por descubrir, ¡y todas las semanas se agregan más películas y series!"
+    },
+    {
+      title: "¿Cuánto cuesta Netflix?",
+      description: "Disfruta Netflix en tu smartphone, tablet, smart TV, laptop o dispositivo de streaming, todo por una tarifa plana mensual. Planes desde $6.540 hasta $11.790 al mes. Sin costos adicionales ni contratos."
+    },
+    {
+      title: "¿Dónde puedo ver Netflix?",
+      description: "Disfruta donde quieras, cuando quieras. Inicia sesión en tu cuenta de Netflix para ver contenido al instante a través de netflix.com desde tu computadora personal o en cualquier dispositivo con conexión a internet que cuente con la app de Netflix, como smart TV, smartphones, tablets, reproductores multimedia y consolas de juegos.",
+      description2: "Además, puedes descargar tus series favoritas con iOS, Android o la app para Windows 10. Con la función de descarga, puedes ver donde vayas y sin conexión a internet. Lleva Netflix contigo adonde sea."
+    },
+    {
+      title: "¿Cómo cancelo?",
+      description: "Netflix es flexible. Sin contratos molestos ni compromisos. Cancela la membresía online con solo dos clics. No hay cargos por cancelación. Empieza y termina cuando quieras.",
+
+    },
+    {
+      title: "¿Qué puedo ver en Netflix?",
+      description: "Netflix tiene un amplio catálogo de películas, series, documentales, animes, originales premiados y más. Todo lo que quieras ver, cuando quieras.",
+
+    },
+    {
+      title: "¿Es bueno Netflix para los niños?",
+      description: "La experiencia de Netflix para niños está incluida en la membresía para que los padres tengan el control mientras los peques disfrutan series y películas familiares en su propio espacio.",
+      description2: "Los perfiles para niños incluyen controles parentales protegidos por PIN que te permiten restringir el contenido que pueden ver los niños en función de la clasificación por edad y bloquear determinados títulos que no quieras que los niños vean."
+
+    }
+  ];
+  
+
+  activarButtonInfo(index:number){
+    if (this.buttonInfo === index) {
+      this.buttonInfo = null;
+    } else {
+      this.buttonInfo = index;
+    }    
+  }
 }
